1b4af72162233a21b693710dd508645049456deb
WHartman00 <william.hartman00@gmail.com>
2025-09-04 14:51:00 -0400

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: NextRequest) {
   try {
     // Email to YOU (ThinkFridge team)
     await transporter.sendMail({
-     from: "ThinkFridge" <no-reply@thinkfridge.co>,
+      from: `"ThinkFridge" <no-reply@thinkfridge.co>`, // Fixed the quotes here
       to: process.env.EMAIL_TO, // Update in Vercel to whartman@thinkfridge.co
       subject: `New Inquiry from ${name}`,
       text: `
